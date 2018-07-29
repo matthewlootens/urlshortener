f52ea7ff84abebe22ebfc54a6a02f131c8c2a84c
Matthew Lootens <mlootens@gmail.com>
2018-07-29 18:38:35 -0400

    add comments

--- BaseConverter.py
+++ BaseConverter.py
@@ -1,52 +1,163 @@
-import hashlib
+import hashlib, string
+"""
+A general class for converting decimal integers into a differnt radix
+
+Designed for use with the flask app, urlShorterner, which turns urls
+into a hash value and then uses BaseConverter to convert to a new
+number for use as a shortened url
+
+Setting a radix value or a characterSet is required before calling
+convert, the only public method.
+
+Todo:
+    * allow negative numbers
+    * allow non-decimal numbers to be entered as strings and literals
+    (i.e., 0xa3c8)
+"""
 class BaseConverter:
     def __init__(self):
-        # self.base = base
-        # self.baseCharacters = characters
-        pass
+        self._radix = None
+        self._characters = None
 
-    def findHighestPower(self, number, base, power = 0):
-        if number // (base ** power) < base:
+    def _findHighestPower(self, number, power = 0):
+        """
+        helper function: finds the number of digits of a
+        decimal number in a base-n number system
+
+        Used in _convertToBase
+        equivalent to: Math.log(number, self._radix) + 1
+
+        @param number The integer (i.e., decimal) number to be converted
+        @param power A starting value for the recursive call
+        @return An integer value
+        """
+        if number // (self._radix ** power) < self._radix:
             return power
-        return self.findHighestPower(number, base, power + 1)
+        return self._findHighestPower(number, power + 1)
 
-    def convertToBase(self, number, power, base):
+    def _convertToBase(self, number, power):
+        """
+        helper function for convert
+        @param number The integer to be converted
+        @param power The number of digits
+        @return string A string: represenation fo the base-n number
+        """
         if power == 0:
-            lsd = number // base
-            return self._baseCharacters[lsd]
+            return self._characters[number]
         else:
-            msd = number // (base ** power)
-            return self._baseCharacters[msd] + self.convertToBase(
-                    number - (msd * base**power),
-                    power - 1, base)
+            msd = number // (self._radix ** power)
+            return self._characters[msd] + self._convertToBase(
+                    number - (msd * self._radix**power),
+                    power - 1)
 
-    def convert(self, number):
-        highestPower = self.findHighestPower(number, self._base)
-        return self.convertToBase(number, highestPower,self. _base)
+    def convert(self, number, inputRadix=10):
+        """
+        the main public method
+        converts a number of the inputRadix to a base-BaseConverter.radix
+        nubmer in string represenation form
 
-    def setBaseCharacters(self, characters):
-        self._baseCharacters = characters
-        self._base = len(characters)
+        BaseConverter.characterSet controls the characters
+        and ordering used
+
+        @param number An integer
+        @param inputBase To extend functionality later
+        @return A string representation of the base-n number
+        @throws AttributeError if no characters have been set
+        """
+        if self._characters is None:
+            raise AttributeError('Must assign a character set first')
+        if number < 0:
+            raise AttributeError('Number must be non-negative')
+        if inputRadix != 10:
+            raise AttributeError('inputBase must equal 10; feature not yet implemented.')
+        highestPower = self._findHighestPower(number)
+        return self._convertToBase(number, highestPower)
 
     @property
-    def base(self):
-        return self._base
+    def radix(self):
+        """the radix/base of the encoding number system"""
+        return self._radix
+
+    @radix.setter
+    def radix(self, newRadix):
+        """
+        if radix is set, a default character set will be assigned from
+        the 62 ascii set of numbers, lowercase, and upercase letters.
+        Ordering will be string.numbers + string.ascii_letters
+
+        @param newRadix an integer in range [2, 62]
+        @throws AttributeError Input is not in range [2, 62]
+        """
+        if newRadix < 2 or newRadix > 62:
+            raise AttributeError('The radix must be betwen 2 and 62.')
+        self._radix = newRadix
+        fullCharSet = string.digits + string.ascii_letters
+        self._characterSet = fullCharSet[:40]
 
     @property
     def characterSet(self):
-        return self._baseCharacters
+        return self._characters
+
+    @characterSet.setter
+    def characterSet(self, characters):
+        """
+        characterSet controls which characters and the total
+        ordering are used in the new
+        base-BaseConverter.radix number
+
+        @param characters A string of unique chacters
+        @throws AttributeError len(characters) is out of range: [2, 62]
+        @throws AttributeError characters contains duplicates
+        """
+        # check for proper length and duplicates
+        if len(characters) < 2 or len(characters) > 62:
+            raise AttributeError('The character string must be between 2 and 62.')
+        if len(characters) != len(set([x for x in characters])):
+            raise AttributeError('The character string has duplicate characters.')
+        self._characters = characters
+        self._radix = len(characters)
 
 if __name__ == "__main__":
     # basic test suite
-    CHARACTERS = '01234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    url = 'http://www.somename.com/'
+    CHARACTERS = string.digits + string.ascii_letters
     converter = BaseConverter()
-    converter.setBaseCharacters(CHARACTERS)
+    try:
+        converter.convert(656)
+    except AttributeError as e:
+        print('Caught attempted conversion without specifiying a radix: ', e)
 
-    m = hashlib.sha256()
-    m.update(url.encode('utf-8'))
+    converter.characterSet = CHARACTERS
 
-    hashVal = m.hexdigest()[:8]
-    hashCode = converter.convert(int(hashVal, 16))
-    print(converter.base)
-    print(hashVal, hashCode)
+    # change characterSet and check again
+    converter.characterSet = '01'
+    assert converter.convert(656) == '1010010000'
+
+    # set another characterSet
+    converter.characterSet = '0123456789ABCDEF'
+    assert converter.convert(656) == '290'
+
+    try:
+        converter.characterSet = CHARACTERS + ',.?'
+    except AttributeError as e:
+        print("Successfully caught string outside of range [2, 62]: ", e)
+
+    try:
+        converter.characterSet = '01234567890'
+    except AttributeError as e:
+        print("Successfully caught string with duplicate: ", e)
+
+    # mutate converting by assinging new radix
+    converter.radix = 2
+    assert converter.convert(656) == '1010010000'
+    converter.radix = 8
+    assert converter.convert(656) == '1220'
+
+    try:
+        converter.radix = 1;
+    except AttributeError as e:
+        print("Successfully caught radix assignment error: ", e)
+
+    try:
+        converter.convert(-10)
+    except AttributeError as e:
+        print("Successfully caught negative input to convert: ", e)
--- urlShortener.py
+++ urlShortener.py
@@ -1,3 +1,17 @@
+"""
+Todo:
+    * redirect only works if url has scheme included; need to auto add http
+    * normalize URLs?
+        * lowercasae scheme and host
+        * captilize escape sequences
+        * remove default port
+
+        Doesn't necessarily preserve original URL semantics
+        * strip leading and trailing whitespace
+        * strip trailing slash
+        * store protocol, hostname, and filename separately?
+"""
+
 from flask import Flask, send_from_directory, redirect, url_for, request, render_template
 import sqlite3, sys, hashlib
 from BaseConverter import BaseConverter
@@ -6,6 +20,9 @@ app = Flask(__name__)
 CHARACTERS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLNOPQRSTUVWXYZ"
 DOMAIN = 'http://localhost:5000/'
 DBFILENAME = './url.db'
+
+converter = BaseConverter()
+converter.characterSet = CHARACTERS
 
 @app.route('/')
 def root():
@@ -16,10 +33,6 @@ def queryDatabase():
     # parse request to get url
     # catch KeyError
     url = request.form['url']
-
-    # generate a hash value
-    converter = BaseConverter()
-    converter.setBaseCharacters(CHARACTERS)
 
     m = hashlib.sha256()
     m.update(url.encode('utf-8'))
@@ -49,5 +62,4 @@ def redirect2(shortenedURL):
     return redirect(fullURL[0])
 
 if __name__ == '__main__':
-    # filename = sys.argv[1]
     app.run(port = 8080)
